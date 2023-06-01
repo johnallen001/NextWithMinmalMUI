c3fa4622c0f66ae4d7f760f6af2ddc9e96ee14a3
johnallen001 <johnallenvillanueva01@gmail.com>
2023-06-01 11:32:16 +0800

    Remove comment

--- src/config-global.js
+++ src/config-global.js
@@ -1,9 +1,6 @@
 // routes
 import { PATH_DASHBOARD } from './routes/paths';
 
-// API
-// ----------------------------------------------------------------------
-
 export const HOST_API_KEY = process.env.HOST_API_KEY || '';
 
 export const FIREBASE_API = {
